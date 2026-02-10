cb3d7a7442bab70cf2237fbf50bc82d797e6b3d8
israkkh <israkkh678@gmail.com>
2026-02-11 00:30:38 +0600

    Initial commit: client + server

---
+++ coffe_store_client/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <h1 className='6xl text-purple-600'>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur praesentium nisi laudantium quasi, optio, labore inventore quibusdam eaque cupiditate quae fugit officia. Odio deserunt quas atque saepe earum repellat corporis.
+      </h1>
+    </div>
+  )
+}
+
+export default App
---
+++ coffe_store_client/src/main.jsx
@@ -0,0 +1,42 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import App from './App.jsx'
+
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+
+import AddCoffee from './components/AddCoffee.jsx'
+import UpdateCoffe from './components/UpdateCoffe.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path: "add-coffee",
+    element: <AddCoffee></AddCoffee>,
+
+  },
+  {
+    path: "update-coffee",
+    element: <UpdateCoffe></UpdateCoffe>,
+
+  },
+]);
+
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
